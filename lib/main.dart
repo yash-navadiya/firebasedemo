193e8b95d703af4a57fd8a8a83d55d264fd8082f
APPLE <apple@APPLEs-MacBook-Pro.local>
2020-06-01 18:04:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfirebasedemo/ceudapp/crud_sample.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = new FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+  new FirebaseAnalyticsObserver(analytics: analytics);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorObservers: <NavigatorObserver>[observer],
+      // home: new WallScreen(analytics: analytics, observer: observer),
+      home: new CrudSample(),
+    );
+  }
+}
